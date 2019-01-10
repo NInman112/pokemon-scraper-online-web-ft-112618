6f2d8b57e182c9a630664eec92d58fbfb592c492
Nick Inman <nick@theoatmeal.com>
2019-01-10 03:22:17 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -21,7 +21,6 @@ class Pokemon
 
     def alter_hp(new_hp, db)
       health = db.execute("SELECT hp FROM pokemon WHERE id = ?", self.id)
-      binding.pry
       damage = (health - new_hp)
       db.execute("UPDATE pokemon SET hp = (hp - ?) WHERE id = ?", damage, self.id)
       #binding.pry
