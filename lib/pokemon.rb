808dfb2d7674e7925c0b7a8cbc7afba4852edc1a
Nick Inman <nick@theoatmeal.com>
2019-01-10 03:17:47 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,27 @@
 class Pokemon
+  attr_accessor :id, :name, :type, :hp, :db
+
+  def initialize(id: id, name: name, type: type, hp: hp, db: db)
+    @id = id
+    @name = name
+    @type = type
+    @hp = hp
+    @db = db
+  end
+
+    def self.save(name, type, db)
+      db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
+      #binding.pry
+    end
+
+    def self.find(p_id, db)
+      poke = db.execute("SELECT * FROM pokemon WHERE id = ?", [p_id]).flatten
+      Pokemon.new(id: poke[0], name: poke[1], type: poke[2])
+    end
+
+    def alter_hp(new_hp, db)
+      #damage = (60 - new_hp)
+      db.execute("UPDATE pokemon SET hp = ? WHERE id = ?", new_hp, self.id)
+      #binding.pry
+    end
 end
